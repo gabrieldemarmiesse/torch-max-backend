48ea81f9d870b0ae24cc170a5f8ce2191df228e7
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2025-08-07 15:44:40 +0000

    Rename file

--- max_torch_backend/compiler.py
+++ max_torch_backend/compiler.py
@@ -101,7 +101,6 @@ class MaxCompiler:
     def __init__(
         self, gm: torch.fx.GraphModule, example_inputs: list[torch.Tensor], mode
     ):
-        print(mode)
         self.gm = gm
         self.example_inputs = example_inputs
         # gm.graph.print_tabular()
