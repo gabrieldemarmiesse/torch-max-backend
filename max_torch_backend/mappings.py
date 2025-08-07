b3e7077a3e9a09b03b5c6a0825b98494466168bd
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2025-08-07 12:34:10 +0000

    Added conv2d

--- max_torch_backend/mappings.py
+++ max_torch_backend/mappings.py
@@ -31,9 +31,26 @@ def torch_cat_equivalent(tensors: list, dim=0):
 def torch_conv2d_equivalent(
     input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1
 ):
-    raise NotImplementedError(
-        "The MAX equivalent of F.conv2d is not implemented yet. Feel free to contribute!"
+    if groups != 1:
+        raise NotImplementedError("Grouped convolution is not supported yet.")
+    
+    if isinstance(stride, int):
+        stride = (stride, stride)
+    if isinstance(padding, int):
+        padding = (padding, padding, padding, padding)
+    elif isinstance(padding, str):
+        raise ValueError("Padding must be an int or a tuple of ints.")
+    elif isinstance(padding, tuple):
+        padding = (padding[0], padding[1], padding[0], padding[1])
+    if isinstance(dilation, int):
+        dilation = (dilation, dilation)
+
+    return max.graph.ops.conv2d(
+        input, weight, bias=bias, stride=stride, padding=padding, dilation=dilation,
+        input_layout=max.graph.type.ConvInputLayout.NCHW, 
+        filter_layout=max.graph.type.FilterLayout.FCRS,
     )
+
 
 
 MAPPING_TORCH_TO_MOJO_FUNCTIONS = {
