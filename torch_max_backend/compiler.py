c7b03d7ffe6096a227e2ff71120865e0252041c1
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2025-08-23 13:08:25 +0000

    Reverse device order

--- torch_max_backend/compiler.py
+++ torch_max_backend/compiler.py
@@ -285,13 +285,16 @@ class _GraphFactory:
 
 
 def get_accelerators() -> list[Device]:
-    result = [CPU()]
+    result = []
     if accelerator_count() > 0:
         for i in range(accelerator_count()):
             try:
                 result.append(Accelerator(i))
             except ValueError as e:
                 warnings.warn(f"Failed to create accelerator {i}. {e}")
+    # This way, people can do torch.device("max_device:0") even if there is
+    # no accelerator and get gpu or cpu automatically.
+    result.append(CPU())
     return result
 
 
