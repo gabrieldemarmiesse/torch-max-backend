8da52e3feea949b3338890d1ca0c18124205e290
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2026-04-21 14:28:16 +0200

    saved

--- tests/test_high_level_ops.py
+++ tests/test_high_level_ops.py
@@ -1511,13 +1511,13 @@ def test_tensor_slice_2d(conf: Conf):
     check_outputs(fn, conf, [x])
 
 
-def test_tensor_slice_negative_index(device: str):
+def test_tensor_slice_negative_index(conf: Conf):
     def fn(x):
         return x[-2:]  # Negative slice
 
     x = torch.randn(5, 3)
 
-    check_functions_are_equivalent(fn, device, [x])
+    check_outputs(fn, conf, [x])
 
 
 def test_tensor_slice_with_step(conf: Conf):
@@ -1529,13 +1529,13 @@ def test_tensor_slice_with_step(conf: Conf):
     check_outputs(fn, conf, [x])
 
 
-def test_to_float(device: str):
+def test_to_float(conf: Conf):
     def fn(x):
         return x.float()
 
     x = torch.randint(0, 10, (5,))
 
-    check_functions_are_equivalent(fn, device, [x])
+    check_outputs(fn, conf, [x])
 
 
 def test_expand_basic(conf: Conf):
@@ -1582,7 +1582,7 @@ def test_expand_same_size(conf: Conf):
     check_outputs(fn, conf, [x])
 
 
-def test_expand_add_dimensions(device: str):
+def test_expand_add_dimensions(conf: Conf):
     """Test expand adding new leading dimensions"""
 
     def fn(x):
@@ -1590,7 +1590,7 @@ def test_expand_add_dimensions(device: str):
 
     x = torch.randn(4)  # 1D tensor
 
-    check_functions_are_equivalent(fn, device, [x])
+    check_outputs(fn, conf, [x])
 
 
 def test_expand_mixed_operations(conf: Conf):
@@ -1617,7 +1617,7 @@ def test_expand_with_scalar_broadcast(conf: Conf):
     check_outputs(fn, conf, [x])
 
 
-def test_expand_complex_pattern(device: str):
+def test_expand_complex_pattern(conf: Conf):
     """Test expand with complex dimension pattern"""
 
     def fn(x):
@@ -1625,7 +1625,7 @@ def test_expand_complex_pattern(device: str):
 
     x = torch.randn(1, 3, 1, 5)
 
-    check_functions_are_equivalent(fn, device, [x])
+    check_outputs(fn, conf, [x])
 
 
 def test_transpose_2d(conf: Conf):
