a11419c6953915a1bc5e05f9fc1c45935bf76ba7
gabrieldemarmiesse <gabrieldemarmiesse@gmail.com>
2026-04-22 09:34:36 +0000

    Make tests use the cpu more

--- tests/conftest.py
+++ tests/conftest.py
@@ -51,8 +51,8 @@ def device(request, gpu_available: bool):
         # Enable when pytorch supports it
         # Conf("max_device:cpu", True),
         # Conf("max_device:gpu", True),
-        Conf("max_device:cpu", False),
-        Conf("max_device:gpu", False),
+        Conf("max_device:cpu", False)
+        # Conf("max_device:gpu", False),
         # Conf("cpu", True),
         # Conf("cuda", True),
     ]
